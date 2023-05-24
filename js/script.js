8ace26e19a60bb7136cfff267596a359ddd035e0
Roma0391 <109166223+Roma0391@users.noreply.github.com>
2023-05-24 21:31:03 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu_list'),
-        menuItem = document.querySelectorAll('.menu_list_item'),
-        hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_list_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu__list_active');
-        })
-    })
+document.getElementById('burger').addEventListener('click', () => {
+    document.getElementById('burger').classList.toggle('menu_mobile_burger_close')
+    document.getElementById('menu_toggle').classList.toggle('menu_items_active')
 })
