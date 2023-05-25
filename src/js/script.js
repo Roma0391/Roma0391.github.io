2dd27fa17004642d316ee23d1b29c19e90ea17b0
Roman <yrm1703@gmail.com>
2023-05-25 11:46:43 +0300

    fitst uber commit

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger').addEventListener('click', () => {
+    document.getElementById('burger').classList.toggle('menu_mobile_burger_close')
+    document.getElementById('menu_toggle').classList.toggle('menu_items_active')
+})
